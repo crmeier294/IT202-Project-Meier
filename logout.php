c03eeaf0f1ec5ff8f50a95dfb053500f5aa2b255
crmeier294 <35932534+crmeier294@users.noreply.github.com>
2018-02-26 17:39:13 -0500

    changed logout redirection

--- logout.php
+++ logout.php
@@ -6,6 +6,6 @@ include("account.php");
 
 session_unset();	//destroy session variables
 session_destroy();	//destroys session
-redirect("Logging out. One moment please.", "login.html");	//return to login.html
+redirect("Logging out. One moment please.", "http://localhost/IT202-Project-Meier/login.html");	//return to login.html
 
 ?>
