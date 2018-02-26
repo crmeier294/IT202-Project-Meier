28661ba5855f6e8176144a66a696dbcbdb4e9d5c
crmeier294 <35932534+crmeier294@users.noreply.github.com>
2018-02-26 17:26:41 -0500

    modified server host-localhost

--- account.php
+++ account.php
@@ -1,8 +1,8 @@
 <?php
 
-$hostname = "sql1.njit.edu"; 			// or "sql2.njit.edu"   OR "SQL1.NJIT.EDU"
-$username = "crm45";
-$project  = "crm45";
-$password = "allstar88";
+$hostname = "localhost"; 			// server
+$username = "root";
+$project  = "Meier";
+$password = "mysql";
 
 ?>
